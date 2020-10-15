c7ef8a7ec577371f35077dc929c81d500958382a
NickHaggerty <Nickhaggerty28@gmail.com>
2020-10-15 17:04:38 -0600

    Update urls.py
    updated to read html files

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import PostListView
 urlpatterns = [
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('', PostListView.as_view(), name='post_list')
     ]
