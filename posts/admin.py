2b6f87dbd09b178c6811102bf7f0396944f8beb0
NickHaggerty <Nickhaggerty28@gmail.com>
2020-10-22 18:39:09 -0600

    finishing up basics
    finished up basic layout of the app itself.

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
 
-# Register your models here.
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInLine,
+        ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
