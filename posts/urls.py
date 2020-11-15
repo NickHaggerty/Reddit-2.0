0960afa61a9a23d6a5dab47a70ec14f353abae6b
NickHaggerty <Nickhaggerty28@gmail.com>
2020-11-15 00:55:25 -0700

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -11,7 +11,7 @@ from .views import (
 urlpatterns = [
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/comment/', PostCommentView.as_view(), name='add_comment'),
+    path('<int:pk>/comment/', PostCommentView.as_view(), name='add_comment'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('new/', PostCreateView.as_view(), name='post_new'),
     path('', PostListView.as_view(), name='post_list')
